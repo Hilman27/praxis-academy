4e495f8f3fc0d6eb5389cb6e7d261c9a2837627d
Hilman27 <57260737+Hilman27@users.noreply.github.com>
2019-11-03 05:21:09 +0700

    Kemampuan Dasar1 Selesai

---
+++ kemampuan-dasar-1/kasus/file-ren.ps1
@@ -0,0 +1,17 @@
+﻿#Get-ChildItem -Path E:\Tugas-tugas\praxis-academy\kemampuan-dasar-1\latihan\powershellscripts -Name
+$filenames = Get-ChildItem -Path $pwd -Name "*.java" | Select-Object -First 1
+if ($filenames){
+ Write-Host "Ada File Java di direktori" $pwd
+ $confirmation = Read-Host "Rename file Java? (y/n)"
+ if ($confirmation -eq 'y') {
+    $newname = Read-Host "Masukan Nama Baru:"
+    Rename-Item $pwd"\"$filenames $newname".java" 
+    #Write-Host $pwd"\"$filenames $newname
+ }
+ 
+}
+else {
+ Write-Host "Tidak ada File Java di direktori" $pwd
+}
+
+Write-Host "Proses Selesai."
